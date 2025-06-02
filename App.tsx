38aee9344072af8d0aeda73db21398282c2e365b
Ashnoor Kaur <Ashnoor.kaur@edu.sait.ca>
2025-06-02 14:48:11 -0600

    Final update App.tsx

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, Text, Image, Button, Alert, StyleSheet } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
-
+import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
 
 export default function App() {
   const handlePress = () => {
@@ -24,7 +24,7 @@ export default function App() {
       </View>
       <View style={styles.lineDivider} />
 
-       {/* User Info */}
+      {/* User Info */}
       <View style={styles.userRow}>
         <Image
           source={{ uri: 'https://t4.ftcdn.net/jpg/02/79/66/93/360_F_279669366_Lk12QalYQKMczLEa4ySjhaLtx1M2u7e6.jpg' }}
@@ -50,9 +50,42 @@ export default function App() {
         <FontAwesome name="bookmark-o" size={24} color="black" />
       </View>
 
+      {/* Like info */}
+      <View style={styles.likeInfoRow}>
+        <Image
+          source={{
+            uri: 'https://t4.ftcdn.net/jpg/02/79/66/93/360_F_279669366_Lk12QalYQKMczLEa4ySjhaLtx1M2u7e6.jpg',
+          }}
+          style={styles.avatarsmall}
+        />
+        <Text style={styles.likeText}>
+          &nbsp;Liked by <Text style={{ fontWeight: 'bold' }}>paisley.print.48</Text> and <Text style={{ fontWeight: 'bold' }}>7 others</Text>.
+        </Text>
+      </View>
 
+      {/* Caption under the post*/}
+      <Text style={styles.caption}>
+        <Text style={{ fontWeight: 'bold' }}>frenchie_fry39 </Text>
+        Fresh shot on a sunny day!
+      </Text>
 
-   {/* Bottom Navigation Bar */}
+      {/* View all comments section*/}
+      <Text style={styles.viewCommentsText}>View all 12 comments</Text>
+
+      {/* Comments */}
+      <Text style={styles.comment}>
+        <Text style={{ fontWeight: 'bold' }}>lil_wyatt838 </Text>
+        Awesome tones
+      </Text>
+      <Text style={styles.comment}>
+        <Text style={{ fontWeight: 'bold' }}>pia.in.a.pod </Text>
+        Gorg. Love it!
+      </Text>
+
+      {/* Time text*/}
+      <Text style={styles.timeText}>1 day ago</Text>
+
+      {/* Bottom Navigation Bar */}
       <View style={styles.lineDividerNavbar} />
       <View style={styles.navbar}>
         <FontAwesome name="home" size={27} color="black" />
@@ -63,9 +96,10 @@ export default function App() {
       </View>
     </View>
 
- );
+  );
 }
- const styles = StyleSheet.create({
+
+const styles = StyleSheet.create({
   // Main container
   container: {
     paddingTop: 1,
@@ -73,6 +107,7 @@ export default function App() {
     backgroundColor: '#fff',
     flex: 1,
   },
+
   // Top Header
   topHeader: {
     flexDirection: 'row',
@@ -89,19 +124,15 @@ export default function App() {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-
   },
-
   smallText: {
     fontSize: 12,
     color: 'gray',
   },
-
   largeText: {
     fontSize: 16,
     fontWeight: 'bold',
   },
-  
   buttonWrapper: {
     marginTop: 2,
     marginRight: 0,
@@ -162,16 +193,57 @@ export default function App() {
     marginRight: 18,
   },
 
+  // Like Info Row
+  likeInfoRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 5,
+  },
+  avatarsmall: {
+    width: 22,
+    height: 20,
+    borderRadius: 50,
+  },
+  likeText: {
+    marginTop: 5,
+  },
 
+  // Caption & Comments section
+  caption: {
+    marginTop: 5,
+  },
+  username: {
+    fontWeight: 'bold',
+  },
+  viewCommentsText: {
+    marginTop: 3,
+    color: 'gray',
+    fontSize: 14,
+  },
+  comment: {
+    marginTop: 3,
+  },
+  timeText: {
+    marginTop: 5,
+    color: 'gray',
+    fontSize: 12,
+  },
 
   // Bottom Navigation
   lineDividerNavbar: {
-
+    height: 1,
+    backgroundColor: '#e0e0e0',
+    width: '100%',
+    marginTop: 12,
+    marginBottom: 5,
   },
   navbar: {
-
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 5,
   },
   navbarIcon: {
-
+    color: "black",
+    fontSize: 24,
   },
 });
